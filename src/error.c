e397e74c1f03a57c09a84d30986a2c1e6cd51e62
Jose carlos Limones hernandez <jlimones@c3r3s3.42malaga.com>
2023-02-04 11:29:47 +0100

    2 entrega 2

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: jlimones <jlimones@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 11:48:09 by jlimones          #+#    #+#             */
-/*   Updated: 2023/02/04 11:24:39 by jlimones         ###   ########.fr       */
+/*   Updated: 2023/02/04 11:28:54 by jlimones         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 /**
  * @brief Funcion error personalizado
  * 
- * @param p_map se pasa el puntero mlx para cerrar le loop correctamente
  * @param error String a mostrar por terminal
  */
 void	ft_error(char *error)
